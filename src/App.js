b8c3db6e7e55ede55f42d38aa9b71b9540e69fda
KjellBerlin <kjell.lillie-stolze@ampero.ai>
2024-10-05 19:48:59 +0200

    Links to medium and github

--- src/App.js
+++ src/App.js
@@ -4,7 +4,9 @@ function App() {
     return (
         <div className="App">
             <div>Carbonara</div>
-            <div>Coming soon</div>
+            <div>
+                On <a href="https://medium.com/@kjell.lilliestolze" target="_blank" rel="noopener noreferrer" className="link">Medium</a> and <a href="https://github.com/KjellBerlin" target="_blank" rel="noopener noreferrer" className="link">GitHub</a>
+            </div>
         </div>
     );
 }
