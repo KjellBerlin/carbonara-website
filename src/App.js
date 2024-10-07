e1faf87fd443bad12ad7a60a281ed1fb969d7aa9
KjellBerlin <kjell.lillie-stolze@ampero.ai>
2024-10-07 16:17:47 +0200

    Improve description

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
                 On <a href="https://medium.com/@kjell.lilliestolze" target="_blank" rel="noopener noreferrer" className="link">Medium</a> and <a href="https://github.com/KjellBerlin" target="_blank" rel="noopener noreferrer" className="link">GitHub</a>
             </div>
             <div>
-                The Carbonara App is a full-stack project designed to demonstrate how to build a complete product with limited resources. Based on an imaginary meal delivery concept, it integrates various technologies, including a frontend, backend, database, authentication, and payment processing. While it's not a functional delivery app, it showcases the practical challenges and solutions in modern app development.
+                The Carbonara App is a full-stack project designed to demonstrate how to build an end to end product with limited resources. Based on an imaginary meal delivery concept, it integrates various technologies, including a frontend, backend, database, authentication, and payment processing. While it's not a functional delivery app, it showcases the practical challenges and solutions in modern app development.
             </div>
         </div>
     );
